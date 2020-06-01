a39ab6422828252fbac1ef18a0f1f330722cb186
Dawud Ibrahim Ismail <ismaildawud96@gmail.com>
2020-06-01 10:05:59 +0000

    added more dummy data (#3)
    * remove accountId connectable
    
    * access trade host using the gateway
    
    * access trade host using the gateway
    
    * added moredummy data

---
+++ src/main/java/com/turntabl/api/service/TradesDemoData.java
@@ -0,0 +1,193 @@
+package com.turntabl.api.service;
+
+import com.turntabl.api.domain.Account;
+import com.turntabl.api.domain.ProductDetail;
+import com.turntabl.api.domain.Side;
+import com.turntabl.api.domain.Trade;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TradesDemoData {
+    List<Trade> trades = new ArrayList<>();
+
+    public TradesDemoData() {
+        //Dummy data
+        // Products Details
+        ProductDetail p1 = new ProductDetail();
+        p1.setExchange("LSE");
+        p1.setProductId("P987");
+        p1.setTicker("SGE");
+
+        ProductDetail p2 = new ProductDetail();
+        p2.setExchange("LSE");
+        p2.setProductId("P556");
+        p2.setTicker("0AI4");
+
+        ProductDetail p3 = new ProductDetail();
+        p3.setExchange("EPA");
+        p3.setProductId("P999");
+        p3.setTicker("ORA");
+
+        ProductDetail p4 = new ProductDetail();
+        p4.setExchange("NASDAQ");
+        p4.setProductId("P387");
+        p4.setTicker("GOOGL");
+
+        ProductDetail p5 = new ProductDetail();
+        p5.setExchange("NYSE");
+        p5.setProductId("P225");
+        p5.setTicker("CRM");
+
+        ProductDetail p6 = new ProductDetail();
+        p6.setExchange("NASDAQ");
+        p6.setProductId("P821");
+        p6.setTicker("ZM");
+
+        ProductDetail p7 = new ProductDetail();
+        p7.setExchange("ASX");
+        p7.setProductId("P764");
+        p7.setTicker("XRO");
+
+
+        // Account
+        Account a1 = new Account();
+        a1.setAccountId("ACC1");
+        a1.setAccountType("Client");
+        a1.setBookName("Euro Trades");
+
+        Account a2 = new Account();
+        a2.setAccountId("ACC2");
+        a2.setAccountType("Offshore");
+        a2.setBookName("Dollar Trades");
+
+        Account a3 = new Account();
+        a3.setAccountId("ACC3");
+        a3.setAccountType("Special");
+        a3.setBookName("Tech Trades");
+
+        Account a4 = new Account();
+        a4.setAccountId("ACC4");
+        a4.setAccountType("Ordinary");
+        a4.setBookName("Foreign Trades");
+
+
+        // Trades
+        Trade t1 = new Trade();
+        t1.setPrice(new BigDecimal("10.20"));
+        t1.setQuantity(100);
+        t1.setSide(Side.BUY);
+        t1.setTradeId("T12345");
+        t1.setAccount(a1);
+        t1.setProductDetail(p3);
+        trades.add(t1);
+
+        Trade t2 = new Trade();
+        t2.setAccount(a1);
+        t2.setProductDetail(p2);
+        t2.setTradeId("T12346");
+        t2.setSide(Side.SELL);
+        t2.setQuantity(5);
+        t2.setPrice(new BigDecimal("676.50"));
+        trades.add(t2);
+
+
+        Trade t3 = new Trade();
+        t3.setPrice(new BigDecimal("179.89"));
+        t3.setQuantity(53);
+        t3.setSide(Side.BUY);
+        t3.setTradeId("T12347");
+        t3.setAccount(a3);
+        t3.setProductDetail(p6);
+        trades.add(t3);
+
+        Trade t4 = new Trade();
+        t4.setAccount(a3);
+        t4.setProductDetail(p4);
+        t4.setTradeId("T12348");
+        t4.setSide(Side.BUY);
+        t4.setQuantity(25);
+        t4.setPrice(new BigDecimal("1431.50"));
+        trades.add(t4);
+
+        Trade t5 = new Trade();
+        t5.setPrice(new BigDecimal("171.72"));
+        t5.setQuantity(532);
+        t5.setSide(Side.SELL);
+        t5.setTradeId("T12349");
+        t5.setAccount(a4);
+        t5.setProductDetail(p5);
+        trades.add(t5);
+
+        Trade t6 = new Trade();
+        t6.setAccount(a4);
+        t6.setProductDetail(p6);
+        t6.setTradeId("T12350");
+        t6.setSide(Side.SELL);
+        t6.setQuantity(105);
+        t6.setPrice(new BigDecimal("178.50"));
+        trades.add(t6);
+
+        Trade t7 = new Trade();
+        t7.setPrice(new BigDecimal("89.20"));
+        t7.setQuantity(100);
+        t7.setSide(Side.BUY);
+        t7.setTradeId("T12351");
+        t7.setAccount(a3);
+        t7.setProductDetail(p7);
+        trades.add(t7);
+
+        Trade t8 = new Trade();
+        t8.setAccount(a1);
+        t8.setProductDetail(p2);
+        t8.setTradeId("T12352");
+        t8.setSide(Side.SELL);
+        t8.setQuantity(23);
+        t8.setPrice(new BigDecimal("676.50"));
+        trades.add(t8);
+
+        Trade t9 = new Trade();
+        t9.setPrice(new BigDecimal("168.98"));
+        t9.setQuantity(150);
+        t9.setSide(Side.BUY);
+        t9.setTradeId("T12353");
+        t9.setAccount(a4);
+        t9.setProductDetail(p5);
+        trades.add(t9);
+
+        Trade t10 = new Trade();
+        t10.setAccount(a3);
+        t10.setProductDetail(p7);
+        t10.setTradeId("T12354");
+        t10.setSide(Side.BUY);
+        t10.setQuantity(47);
+        t10.setPrice(new BigDecimal("85.41"));
+        trades.add(t10);
+
+        Trade t11 = new Trade();
+        t11.setPrice(new BigDecimal("1428.87"));
+        t11.setQuantity(10);
+        t11.setSide(Side.BUY);
+        t11.setTradeId("T12355");
+        t11.setAccount(a2);
+        t11.setProductDetail(p4);
+        trades.add(t11);
+
+        Trade t12 = new Trade();
+        t12.setAccount(a3);
+        t12.setProductDetail(p5);
+        t12.setTradeId("T12356");
+        t12.setSide(Side.SELL);
+        t12.setQuantity(236);
+        t12.setPrice(new BigDecimal("172.52"));
+        trades.add(t12);
+
+    }
+
+    public List<Trade> getTrades() {
+        return trades;
+    }
+}
